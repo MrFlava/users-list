07ff7cbb41a3f7e26667000c176b9cf299a0eacd
MrFlava <thatelitemaili33t@gmail.com>
2023-07-03 10:56:13 +0300

    feat: provide update and delete endpoints for the users management

--- app.py
+++ app.py
@@ -44,12 +44,26 @@ def new_user():
         return jsonify({'username': user.username}), 201, {'Location': url_for('get_user', pk=user.id, _external=True)}
 
 
-@app.route('/api/users/<int:pk>')
+@app.route('/api/users/<int:pk>', methods=['PUT', 'GET', 'DELETE'])
 def get_user(pk: int):
     user = User.query.get(pk)
     if not user:
         abort(400)
-    return jsonify({'username': user.username, 'email': user.email})
+
+    if request.method == 'GET':
+        return jsonify({'username': user.username, 'email': user.email}, 200)
+
+    elif request.method == 'PUT':
+        user.username = request.json.get('username')
+        user.email = request.json.get('email')
+        user.hash_password(request.json.get('password'))
+
+        db.session.commit()
+        return jsonify({'username': user.username, 'email': user.email}, 200)
+
+    else:
+        db.session.delete(user)
+        return jsonify({'data': 'user were deleted'}, 200)
 
 
 @app.route('/api')
