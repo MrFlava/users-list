1d278a5c8b8f9d2d671aeb73ae087adc1a221241
MrFlava <thatelitemaili33t@gmail.com>
2023-07-03 13:34:33 +0300

    style: code-style updates

--- app.py
+++ app.py
@@ -1,79 +1,50 @@
-from os import path
+from flask import Flask, abort, request, jsonify
+from flask.wrappers import Response
 
-from flask import Flask, abort, request, jsonify, g, url_for
-
-from models import User, db
-
-basedir = path.abspath(path.dirname(__file__))
+from models import db, User
+from utils import create_new_user, list_of_users, update_user, delete_user
+from settings import SQLALCHEMY_DATABASE_URI, SQLALCHEMY_TRACK_MODIFICATIONS
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] =\
-        'sqlite:///' + path.join(basedir, 'database.db')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = SQLALCHEMY_TRACK_MODIFICATIONS
 
 db.init_app(app)
 
 
-@app.route('/api/users', methods=['POST', 'GET'])
-def new_user():
-    if request.method == 'GET':
-        user_objects = User.query.all()
-        users = []
-
-        for user in user_objects:
-            users.append({'username': user.username, 'email': user.email})
-
-        return jsonify({'users': users}, 200)
+@app.route("/api/users", methods=["POST", "GET"])
+def get_or_create_users() -> Response:
+    if request.method == "GET":
+        return list_of_users()
 
     else:
-        username = request.json.get('username')
-        email = request.json.get('email')
-        password = request.json.get('password')
-
-        if username is None or password is None or email is None:
-            abort(400)    # missing arguments
-        if User.query.filter_by(username=username).first() is not None:
-            abort(400)    # existing user
-
-        user = User(username=username, email=email)
-        user.hash_password(password)
-
-        db.session.add(user)
-        db.session.commit()
-
-        return jsonify({'username': user.username}), 201, {'Location': url_for('get_user', pk=user.id, _external=True)}
+        return create_new_user(request)
 
 
-@app.route('/api/users/<int:pk>', methods=['PUT', 'GET', 'DELETE'])
-def get_user(pk: int):
+@app.route("/api/users/<int:pk>", methods=["PUT", "GET", "DELETE"])
+def get_user_and_create_or_delete(pk: int) -> Response:
     user = User.query.get(pk)
     if not user:
         abort(400)
 
-    if request.method == 'GET':
-        return jsonify({'username': user.username, 'email': user.email}, 200)
+    if request.method == "GET":
+        return jsonify({"status_code": 200, "user": {"username": user.username, "email": user.email}})
 
-    elif request.method == 'PUT':
-        user.username = request.json.get('username')
-        user.email = request.json.get('email')
-        user.hash_password(request.json.get('password'))
-
-        db.session.commit()
-        return jsonify({'username': user.username, 'email': user.email}, 200)
+    elif request.method == "PUT":
+        return update_user(user, request)
 
     else:
-        db.session.delete(user)
-        return jsonify({'data': 'user were deleted'}, 200)
+        return delete_user(user)
 
 
-@app.route('/api')
-def hello_world():
-    return jsonify({'data': 'Hello, World!'})
+@app.route("/api")
+def hello_world() -> Response:
+    return jsonify({"data": "Hello, World!", "status_code": 200})
 
 
 with app.app_context():
     db.create_all()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
